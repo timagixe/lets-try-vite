8c7ada40b9a00fcd31b6e650e23d3bdee72fdd66
Tima Gixe <timagixe@protonmail.com>
2021-10-21 09:44:49 +0300

    chore: install prettier

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'lf',
+};
